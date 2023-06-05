969ade077b6cc2295ad8990123d91d59abd0698e
sultanali-mughal <sultanalimughal01@gmail.com>
2023-06-05 16:42:08 +0500

    Expend & Collapse

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,32 +14,35 @@ export default function Footer() {
             <a className="link" href="#">STORE LOCATOR</a>
           </div>
           <div class="grid-item">
-            <p className="para">ABOUT GULAHMED</p>
-            <a className="link" href="#">ABOUT US</a>
-            <a className="link" href="#">COMPANY</a>
-            <a className="link" href="#">CAREERS</a>
-            <a className="link" href="#">STORE LOCATOR</a>
+            <p className="para">MY ACCOUNT</p>
+            <a className="link" href="#">LOGIN</a>
+            <a className="link" href="#">CREAT ACCOUNT</a>
+            <a className="link" href="#">ACCOUNT INFO</a>
+            <a className="link" href="#">ORDER HISTORY</a>
           </div>
           <div class="grid-item">
-            <p className="para">ABOUT GULAHMED</p>
-            <a className="link" href="#">ABOUT US</a>
-            <a className="link" href="#">COMPANY</a>
-            <a className="link" href="#">CAREERS</a>
-            <a className="link" href="#">STORE LOCATOR</a>
+            <p className="para">CUSTOMER CARE</p>
+            <a className="link" href="#">FAQ</a>
+            <a className="link" href="#">SERVICES</a>
+            <a className="link" href="#">CONTACT US</a>
+            <a className="link" href="#">VISUAL SEARCH</a>
+            <a className="link" href="#">PRIVACY POLICY</a>
+            <a className="link" href="#">SHIPPING POLICY</a>
+            <a className="link" href="#">REQUEST A CALL BACK</a>
           </div>
           <div class="grid-item">
-            <p className="para">ABOUT GULAHMED</p>
-            <a className="link" href="#">ABOUT US</a>
-            <a className="link" href="#">COMPANY</a>
-            <a className="link" href="#">CAREER'S</a>
-            <a className="link" href="#">STORE LOCATOR</a>
-          </div>
-          <div class="grid-item">
-            <p className="para">ABOUT GULAHMED</p>
-            <a className="link" href="#">ABOUT US</a>
-            <a className="link" href="#">COMPANY</a>
-            <a className="link" href="#">CAREERS</a>
-            <a className="link" href="#">STORE LOCATOR</a>
+            <p className="para">FIND US</p>
+            
+            <i className="fa-brands fa-facebook"></i>
+            <a className="link social" href="#">FACEBOOK</a>
+            <i className="fa-brands fa-instagram"></i>
+            <a className="link social" href="#">INSTAGRAM</a>
+            <i className="fa-brands fa-twitter"></i>
+            <a className="link social" href="#">TWITTER</a>
+            <i className="fa-brands fa-youtube"></i>
+            <a className="link social" href="#">YOUTUBE</a>
+            <i className="fa-brands fa-whatsapp"></i>
+            <a className="link social" href="#">WHATSAPP</a>
           </div>
         </div>
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,160 +1,204 @@
-import React, { useState } from "react";
-import "../assets/css/sidebar.css";
-import Dropdown from "react-bootstrap/Dropdown";
+import React, { useState } from 'react';
 
-function Sidebar() {
-  const [collapseShow, setCollapseShow] = useState("collapse");
-  const [collapseShow1, setCollapseShow1] = useState("collapse");
-  const [collapseShow2, setCollapseShow2] = useState("collapse");
+const CollapseButton = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
-  const [expanded, setExpanded] = useState(true);
-
-  function Collapsible(event) {
-    debugger;
-    if (event.target.id === "btnCollapseOne") {
-      if (expanded === true) {
-        setCollapseShow("collapse show");
-      } else if (expanded === false) {
-        setCollapseShow("collapse");
-      }
-    }
-
-    if (event.target.id === "btnCollapseTwo") {
-      if (expanded === true) {
-        setCollapseShow1("collapse show");
-      } else if (expanded === false) {
-        setCollapseShow1("collapse");
-      }
-    }
-
-    if (event.target.id === "btnCollapseThree") {
-      if (expanded === true) {
-        setCollapseShow2("collapse show");
-      } else if (expanded === false) {
-        setCollapseShow2("collapse");
-      }
-    }
-
-    setExpanded(!expanded);
-  }
+  const handleToggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
   return (
-    <div className="sidebar">
-      <h3>FILTER</h3>
-      <hr />
-      <div id="accordion">
-        <div class="card">
-          <div class="card-header" id="headingOne">
-            <h5 class="mb-0">
-              <button
-                id="btnCollapseOne"
-                class="btn btn-link"
-                data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded={expanded}
-                aria-controls="collapseOne"
-                onClick={(event) => Collapsible(event)}
-              >
-                Collapsible Group Item #1
-              </button>
-            </h5>
-          </div>
-
-          <div
-            id="collapseOne"
-            class={collapseShow}
-            aria-labelledby="headingOne"
-            data-parent="#accordion"
-          >
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </div>
-          </div>
+    <div>
+      <button onClick={handleToggleCollapse}>
+        {isCollapsed ? 'Expand' : 'Collapse'}
+      </button>
+      {!isCollapsed && (
+        <div>
+          {/* Content to be displayed when expanded */}
+          <p>This is the expanded content.</p>
         </div>
-        <div class="card">
-          <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-              <button
-                class="btn btn-link"
-                id="btnCollapseTwo"
-                data-toggle="collapse"
-                data-target="#collapseTwo"
-                aria-expanded={expanded}
-                aria-controls="collapseTwo"
-                onClick={(event) => Collapsible(event)}
-              >
-                Collapsible Group Item #2
-              </button>
-            </h5>
-          </div>
-          <div
-            id="collapseTwo"
-            class={collapseShow1}
-            aria-labelledby="headingTwo"
-            data-parent="#accordion"
-          >
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header" id="headingThree">
-            <h5 class="mb-0">
-              <button
-                class="btn btn-link"
-                id="btnCollapseThree"
-                data-toggle="collapse"
-                data-target="#collapseTwo"
-                aria-expanded={expanded}
-                aria-controls="collapseTwo"
-                onClick={(event) => Collapsible(event)}
-              >
-                Collapsible Group Item #3
-              </button>
-            </h5>
-          </div>
-          <div
-            id="collapseThree"
-            class={collapseShow2}
-            aria-labelledby="headingThree"
-            data-parent="#accordion"
-          >
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-              on it squid single-origin coffee nulla assumenda shoreditch et.
-              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-              lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them
-              accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
-}
+};
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h4>React Collapse/Expand Example</h4>
+      <hr />
+      <CollapseButton />
+      <hr />
+      <CollapseButton />
+      <hr />
+      <CollapseButton />
+      <hr />
+    </div>
+  );
+};
 
 export default Sidebar;
+
+
+
+
+// import React, { useState } from "react";
+// import "../assets/css/sidebar.css";
+// import Dropdown from "react-bootstrap/Dropdown";
+
+// function Sidebar() {
+//   const [collapseShow, setCollapseShow] = useState("collapse");
+//   const [collapseShow1, setCollapseShow1] = useState("collapse");
+//   const [collapseShow2, setCollapseShow2] = useState("collapse");
+
+//   const [expanded, setExpanded] = useState(true);
+
+//   function Collapsible(event) {
+//     debugger;
+//     if (event.target.id === "btnCollapseOne") {
+//       if (expanded === true) {
+//         setCollapseShow("collapse show");
+//       } else if (expanded === false) {
+//         setCollapseShow("collapse");
+//       }
+//     }
+
+//     if (event.target.id === "btnCollapseTwo") {
+//       if (expanded === true) {
+//         setCollapseShow1("collapse show");
+//       } else if (expanded === false) {
+//         setCollapseShow1("collapse");
+//       }
+//     }
+
+//     if (event.target.id === "btnCollapseThree") {
+//       if (expanded === true) {
+//         setCollapseShow2("collapse show");
+//       } else if (expanded === false) {
+//         setCollapseShow2("collapse");
+//       }
+//     }
+
+//     setExpanded(!expanded);
+//   }
+
+//   return (
+//     <div className="sidebar">
+//       <h3>FILTER</h3>
+//       <hr />
+//       <div id="accordion">
+//         <div class="card">
+//           <div class="card-header" id="headingOne">
+//             <h5 class="mb-0">
+//               <button
+//                 id="btnCollapseOne"
+//                 class="btn btn-link"
+//                 data-toggle="collapse"
+//                 data-target="#collapseOne"
+//                 aria-expanded={expanded}
+//                 aria-controls="collapseOne"
+//                 onClick={(event) => Collapsible(event)}
+//               >
+//                 Collapsible Group Item #1
+//               </button>
+//             </h5>
+//           </div>
+
+//           <div
+//             id="collapseOne"
+//             class={collapseShow}
+//             aria-labelledby="headingOne"
+//             data-parent="#accordion"
+//           >
+//             <div class="card-body">
+//               Anim pariatur cliche reprehenderit, enim eiusmod high life
+//               accusamus terry richardson ad squid. 3 wolf moon officia aute, non
+//               cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+//               laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+//               on it squid single-origin coffee nulla assumenda shoreditch et.
+//               Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+//               nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+//               lomo. Leggings occaecat craft beer farm-to-table, raw denim
+//               aesthetic synth nesciunt you probably haven't heard of them
+//               accusamus labore sustainable VHS.
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card">
+//           <div class="card-header" id="headingTwo">
+//             <h5 class="mb-0">
+//               <button
+//                 class="btn btn-link"
+//                 id="btnCollapseTwo"
+//                 data-toggle="collapse"
+//                 data-target="#collapseTwo"
+//                 aria-expanded={expanded}
+//                 aria-controls="collapseTwo"
+//                 onClick={(event) => Collapsible(event)}
+//               >
+//                 Collapsible Group Item #2
+//               </button>
+//             </h5>
+//           </div>
+//           <div
+//             id="collapseTwo"
+//             class={collapseShow1}
+//             aria-labelledby="headingTwo"
+//             data-parent="#accordion"
+//           >
+//             <div class="card-body">
+//               Anim pariatur cliche reprehenderit, enim eiusmod high life
+//               accusamus terry richardson ad squid. 3 wolf moon officia aute, non
+//               cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+//               laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+//               on it squid single-origin coffee nulla assumenda shoreditch et.
+//               Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+//               nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+//               lomo. Leggings occaecat craft beer farm-to-table, raw denim
+//               aesthetic synth nesciunt you probably haven't heard of them
+//               accusamus labore sustainable VHS.
+//             </div>
+//           </div>
+//         </div>
+//         <div class="card">
+//           <div class="card-header" id="headingThree">
+//             <h5 class="mb-0">
+//               <button
+//                 class="btn btn-link"
+//                 id="btnCollapseThree"
+//                 data-toggle="collapse"
+//                 data-target="#collapseTwo"
+//                 aria-expanded={expanded}
+//                 aria-controls="collapseTwo"
+//                 onClick={(event) => Collapsible(event)}
+//               >
+//                 Collapsible Group Item #3
+//               </button>
+//             </h5>
+//           </div>
+//           <div
+//             id="collapseThree"
+//             class={collapseShow2}
+//             aria-labelledby="headingThree"
+//             data-parent="#accordion"
+//           >
+//             <div class="card-body">
+//               Anim pariatur cliche reprehenderit, enim eiusmod high life
+//               accusamus terry richardson ad squid. 3 wolf moon officia aute, non
+//               cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+//               laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+//               on it squid single-origin coffee nulla assumenda shoreditch et.
+//               Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+//               nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+//               lomo. Leggings occaecat craft beer farm-to-table, raw denim
+//               aesthetic synth nesciunt you probably haven't heard of them
+//               accusamus labore sustainable VHS.
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
