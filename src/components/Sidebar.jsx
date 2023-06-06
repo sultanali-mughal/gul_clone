a7f4ea870309717f79e93df2597890f9897f82d3
sultanali-mughal <sultanalimughal01@gmail.com>
2023-06-06 16:19:26 +0500

    push

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
         buttonText="CATEGORY"
         content="> <a href='#'>Summer Esssential Collection</a> <BR>
                  > <a href='#'>Tribute Collection</a> <BR> 
-                 > <a href='#'>Summer Premium Collection</a> <BR>
+                 > <a href='#'>Summer Premium Collections</a> <BR>
                  > <a href='#'>Vintage Garden</a> <BR> 
                  > <a href='#'>Yolo collection</a> <BR>
                  > <a href='#'>Red Collection 2023</a> <BR>
