209eb42c82a76735824fdfe410748e1c059346e1
sultanali-mughal <sultanalimughal01@gmail.com>
2023-06-14 22:43:37 +0500

    add line

--- src/components/AccordionExp.tsx
+++ src/components/AccordionExp.tsx
@@ -13,10 +13,12 @@ function AccordionExp() {
                         <div className="bg-warning border">Second item</div>
                         <div className="bg-warning border">Third item</div>
                     </Stack>
+
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Accordion Item #2</Accordion.Header>
+
                 <Accordion.Body>
                     <Stack gap={3}>
                         <div className="bg-warning border">First item</div>
